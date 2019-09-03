3fefe72b2bf4cd4723e6c2494e4debdf3017bd6e
Emilio Tirado <etirado.ctr@ten-x.com>
2019-09-03 13:49:06 -0400

    1. calling grading api 2. allowing to move through the exercises

--- src/components/Spelling.js
+++ src/components/Spelling.js
@@ -3,34 +3,51 @@ import { useSpeechSynthesis } from 'react-speech-kit';
 import SpellingExercise from './SpellingExercise'
 import UnsupportedBrowser from './UnsupportedBrowser'
 import Loading from './Loading'
+import Message from './Message'
 import { SpellingProvider } from '../providers'
+import { GRADES, SPELLING_LEVELS, GRADE_APPROVED_MSG, GRADE_WARNING_MSG, GRADE_REJECTED_MSG } from '../constants'
 
 const Speech = () => {
   const { supported, speak } = useSpeechSynthesis()
-  const [exercises, setExercises] = useState([])
+  const [exercise, setExercise] = useState({})
   const [current, setCurrent] = useState(0)
   const [loading, setLoading] = useState(true)
+  const [msg, setMsg] = useState(null)
   
   useEffect(() => {
     const fetchApi = async () => {
       const data = await SpellingProvider.fetchExercises({})
-      setExercises(data)
+      setExercise(data)
       setCurrent(0)
       setLoading(false)
     }
     fetchApi()
   }, [])
 
+  const gradeResult = async (userInput) => {
+    setLoading(true)
+    const result = await SpellingProvider.gradeExercise({...exercise,  userInput})
+    setExercise(result.next)
+    if (result.grades === GRADES.APPROVED) {
+      setMsg(GRADE_APPROVED_MSG)
+    } else if (result.grades === GRADES.WARNING) {
+      setMsg(GRADE_WARNING_MSG)
+    } else if (result.grades === GRADES.REJECTED) {
+      setMsg(GRADE_REJECTED_MSG)
+    }
+    setLoading(false)
+  }
+
   if (!supported) {
     return <UnsupportedBrowser />
   }
 
-  const exercise = exercises[current] || ''
   return (
     <div>
+      {msg && <Message msg={msg} />}
       {loading ?
         <Loading /> :
-        <SpellingExercise text={exercise} speak={speak} gradeResult={() => console.warn('TODO: send data to grade')} />
+        <SpellingExercise text={exercise.text || ''} speak={speak} gradeResult={gradeResult} />
       }
     </div>
   )
